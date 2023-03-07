341c11b0c2e5407479370db3ec910b67aba01375
Crypto-Path <60126762+Crypto-Path@users.noreply.github.com>
2023-03-07 09:40:14 -0800

    Basic Game Uploaded
    Playable

---
+++ Scripts/chart.js
@@ -0,0 +1,130 @@
+class Chart {
+    /**
+     * Description
+     * @param {*} map 
+     * @param {*} rows 
+     * @param {*} xOffset 
+     * @param {*} yOffset 
+     * @param {*} xVel 
+     * @param {*} yVel 
+     * @param {*} progress 
+     */
+    constructor(map, rows, xOffset = 0, yOffset = 0, xVel = 0, yVel = 0, progress = 0) {
+        this.map = map;
+        this.rows = rows;
+        this.xOffset = xOffset;
+        this.yOffset = yOffset;
+        this.xVel = xVel;
+        this.yVel = yVel;
+        this.progress = progress;
+
+        this.row = []
+        /*Sets each row to an empty array for some purpose idk*/
+        for (let i = 0; i < rows; i++) {
+            this.row[i] = [];
+        }
+
+        this.Load_Map()
+    }
+
+    Load_Map() {
+        const mapChart = this.map["4Rows"]["Difficulty1"];
+        for (let i in mapChart) {
+            for (let j in i) {
+                try {
+                    console.log(j)
+                    const notes = this.map["4Rows"]["Difficulty1"][i][j];
+                    console.log(notes);
+                    for (let x = 0; x < notes.length; x++) {
+                        const element = notes[x];
+                        console.log(element)
+                        this.tempNote = new Note(j*100,-element/(parseFloat(this.map["BPM"])*4),this.row[j].length) // Creates note object
+                        try { // A previous note doesn't always exist
+                            this.tempNote.Set_Start_Pos([this.tempNote.Get_Pos()[0], this.tempNote.Get_Pos()[1] + (this.row[j][this.row[j].length-1].Get_Pos()[1])]); // Sets note y offset of previous note ([10,10] -> [10,20]) to avoid overlap and reduce map size
+                            this.tempNote.Set_Pos(this.tempNote.Get_Start_Pos());
+                        } catch (error) { }
+                        this.Append(this.tempNote,j) // Appends note to row array which holds all notes
+                    }
+                } catch (error) {
+                    return
+                }
+            }
+        }
+    }
+
+    /**
+     * Returns map data (Map Name, Map Create, Etc.)
+     */
+    Get_MetaData() {
+        return [this.map["Name"],this.map["Creator"],this.map["BPM"]]
+    }
+
+    /**
+     * Adds note to chart
+     * @param note The note object itself
+     * @param row The row the note is appended to || Default: 0
+     */
+    Append(note, row = 0) {
+        this.row[row].push(note); // Adds Note object to an array inside the rows array (EX: rows = [[note,note,note],[note,note]])
+    }
+
+    Remove(row) {
+        this.row[row].shift();
+    }
+
+    UpdateNotes(row = 0) {
+        for (let i = 0; i < this.row[row].length; i++) {
+            const note = this.row[row][i];
+            note.Set_Pos([note.Get_Pos()[0], note.Get_Start_Pos()[1] + this.yVel * this.progress]);
+        }
+    }
+    //Update
+
+    Get_Next(row) {
+
+    }
+
+    Get_Rows() {
+        return this.rows;
+    }
+
+    Get_Poss(row) {
+        return this.row[row];
+    }
+
+    Get_Progress() {
+        return this.progress;
+    }
+
+    Set_Progess(progress) {
+        this.progress = progress
+    }
+
+    ChangeProgress(change) {
+        this.progress += change;
+    }
+
+    Get_Xs(row) {
+
+    }
+
+    Get_Ys(row) {
+
+    }
+
+    Set_X_Offet(xOffset) {
+
+    }
+
+    Set_Y_Offet(yOffset) {
+        
+    }
+
+    Set_X_Vel(xVel) {
+        
+    }
+
+    Set_Y_Vel(yVel) {
+        
+    }
+}
---
+++ Scripts/note.js
@@ -0,0 +1,43 @@
+class Note {
+    /**
+     * Description:
+     * The most basic element, a note. Provides its location and data, and its availibilty for those to be change.
+     * @param {*} x 
+     * @param {*} y 
+     * @param {*} id 
+     */
+    constructor(x,y,id) {
+        this.start_x = x;
+        this.start_y = y;
+        this.x = this.start_x;
+        this.y = this.start_y;
+        this.id = id;
+    }
+
+    Get_Start_Pos() {
+        return [this.start_x, this.start_y];
+    }
+
+    Set_Start_Pos(pos) {
+        this.start_x = pos[0];
+        this.start_y = pos[1];
+    }
+
+    Get_Pos() {
+        return [this.x, this.y];
+    }
+
+    Set_Pos(pos) {
+        this.x = pos[0];
+        this.y = pos[1]
+    }
+
+    Change_Pos(change) {
+        this.x += change[0];
+        this.y += change[1];
+    }
+
+    Get_Id() {
+        return this.id;
+    }
+}
