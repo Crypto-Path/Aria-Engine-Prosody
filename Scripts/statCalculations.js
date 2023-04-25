a68b13aee12b3957ad36cdc60094ac476abfbdbe
Crypto-Path <cyphe371@gmail.com>
2023-04-25 12:06:54 -0700

    a

--- Scripts/statCalculations.js
+++ Scripts/statCalculations.js
@@ -23,9 +23,9 @@ function Get_Score(_judgements, _judgementsWeights, _combosHighest) {
     let _HighestC = _combosHighest[2];
 
     let _PPScore = (_HighestPP + Judgements[0])/2/_totalNotes
-    let _PScore = ((_HighestP - _HighestPP + Judgements[1])/2)/(_totalNotes*1.5)
-    let _CScore = ((_HighestC - _HighestPP + Judgements[2])/2)/(_totalNotes*2)
-    let _HScore = ((_hitNotes - _HighestPP + Judgements[3])/2)/(_totalNotes*3)
+    let _PScore = ((_HighestP - _HighestPP + Judgements[1])/2)/(_totalNotes*2)
+    let _CScore = ((_HighestC - _HighestPP + Judgements[2])/2)/(_totalNotes*3)
+    let _HScore = ((_hitNotes - _HighestPP + Judgements[3])/2)/(_totalNotes*4)
     console.log(_PPScore);
     console.log(_PScore);
     console.log(_CScore);
