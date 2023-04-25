513e99f0361eebfe21e47cd09f8d2582b8c304d4
Crypto-Path <cyphe371@gmail.com>
2023-04-25 16:24:17 -0700

    Changed how maps read
    instead of a note being recognized as the distance it is from the previous not it is not the distance from the start of the map

--- Scripts/chart.js
+++ Scripts/chart.js
@@ -25,7 +25,7 @@ class Chart {
         /*Sets each row to an empty array for some purpose idk*/
         for (let i = 0; i < rows; i++) {
             this.row[i] = [];
-            this.map["4Rows"]["Difficulty1"]["Notes"][i][0] += this.map["4Rows"]["Difficulty1"]["Offset"];
+           // this.map["4Rows"]["Difficulty1"]["Notes"][i][0];
         }
 
 
@@ -40,11 +40,9 @@ class Chart {
                 const notes = mapChart[i];
                 for (let x = 0; x < notes.length; x++) {
                     const element = notes[x];
-                    this.tempNote = new Note(i*100, -element/this.rate,this.row[i].length) // Creates note object
-                    try { // A previous note doesn't always exist
-                        this.tempNote.Set_Start_Pos([this.tempNote.Get_Pos()[0], (this.tempNote.Get_Pos()[1]) / (this.map["BPM"] / 60) /* /4 because 4/4 music notation */ + (this.row[i][this.row[i].length-1].Get_Pos()[1]) /* Gets previous not position */]); // Sets note y offset of previous note ([10,10] -> [10,20]) to avoid overlap and reduce map size
-                        this.tempNote.Set_Pos(this.tempNote.Get_Start_Pos());
-                    } catch (error) { }
+                    this.tempNote = new Note(i*100, element/this.rate,this.row[i].length) // Creates note object
+                    this.tempNote.Set_Start_Pos([this.tempNote.Get_Pos()[0], -(this.tempNote.Get_Pos()[1]) / (this.map["BPM"] / 60) - this.map["4Rows"]["Difficulty1"]["Offset"] / this.rate /* this.rate/*+ (this.row[i][this.row[i].length].Get_Pos()[1]) /* Gets previous not position */]); // Sets note y offset of previous note ([10,10] -> [10,20]) to avoid overlap and reduce map size
+                    this.tempNote.Set_Pos(this.tempNote.Get_Start_Pos());
                     this.Append(this.tempNote, i) // Appends note to row array which holds all notes
                 }
             } catch (error) {
