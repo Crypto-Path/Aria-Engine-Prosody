a6c6acac33f610a2694b80694abc46efb5d4100d
Crypto-Path <cyphe371@gmail.com>
2023-09-20 10:18:41 -0700

    Improved server get system

--- Data/Maps.js
+++ Data/Maps.js
@@ -1,16 +1,22 @@
 let charts;
 let _charts = [];
 let _alt = [];
-getCharts();
 
 function getCharts() {
-    fetch("https://corsproxy.io/?https://prosody-server.schiytu37.repl.co/charts/ranked/")
+    fetch("https://corsproxy.io/?https://prosody-server.schiytu37.repl.co/charts/ranked/count.json")
         .then(response => response.text())
         .then(data => {
             console.log(data);
             chartCount = JSON.parse(data)["RankedCharts"];
+
+            let value = 0;
+            const progressBar = document.getElementById("myProgressBar");
+            const progressBarText = document.getElementById("myProgressBarText");
+            progressBar.max = chartCount - 1;
+            progressBarText.innerText = `0/${progressBar.max+1} charts loaded...`;
+
             for (let i = 1; i < chartCount; i++) {
-                fetch("https://corsproxy.io/?https://prosody-server.schiytu37.repl.co/charts/ranked/" + i)
+                fetch(`https://corsproxy.io/?https://prosody-server.schiytu37.repl.co/charts/ranked/${i}/chartData.json`)
                     .then(chart => chart.text())
                     .then(chartData => {
                         skip = false;
@@ -24,20 +30,20 @@ function getCharts() {
                             }
                         }
                         if (!skip) {
+                            value += 1;
+                            progressBar.value = value;
+                            progressBarText.innerText = `${value+1}/${progressBar.max+1} charts loaded...`;
                             _charts.push([chartData, i]);
                         }
 
                         if (i == chartCount - 1) {
                             _alt.length = _charts.length; // Sets the length of _alt so the exact index can be reached
                             console.log(_alt);
-                            for (let j = 0; j < _alt.length; j++) {
-                                _alt[getIndexOfChart(j + 1)] = _charts[j][0];
-                                //console.log(_alt);
-                            }
-                            // if (_alt[-1] != null) {
-                            //     //_alt.shift(); // 
-                            //     console.log("System: Removed negative index")
-                            // }
+                            sortCharts(_charts)
+                                // if (_alt[-1] != null) {
+                                //     //_alt.shift(); // 
+                                //     console.log("System: Removed negative index")
+                                // }
                             console.log(_alt);
                             charts = _alt;
                         }
@@ -62,4 +68,25 @@ function getIndexOfChart(index) {
         }
     }
     return _charts.length;
+}
+
+function sortCharts(toSort) {
+    toSort.sort(function(a, b) {
+        // Compare the second elements (index 1) of each array
+        return a[1] - b[1];
+    });
+    for (let j = 0; j < _alt.length; j++) {
+        _alt[j] = toSort[j][0];
+        //console.log(_alt);
+    }
+}
+
+function reloadChart() {
+    for (let i = 0; i < document.getElementsByClassName("SongSelector").length; i = i) {
+        document.getElementsByClassName("SongSelector")[0].remove();
+    }
+    sortCharts();
+    for (var i in charts) {
+        CreateButton(i)
+    }
 }
